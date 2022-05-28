dc351726d310cac087c1558ac2918143796f9df1
Thierry <marcelinthierry@gmail.com>
2022-05-28 18:48:43 +0800

    Finish Etch-a-Sketch project

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let size = document.querySelector(".div-container").clientWidth;
 
 // value can be passed through text
 function createDivs(value = 16) {
+  divContainer.replaceChildren();
   for (let i = 0; i < value; i++) {
     const divRows = document.createElement("div");
     divRows.classList.add("row");
@@ -13,16 +14,33 @@ function createDivs(value = 16) {
     for (let j = 0; j < value; j++) {
       const div = document.createElement("div");
       div.classList.add("sketch");
-      const divSize = size / value;
+      const divSize = Math.floor(size / value);
       div.style.width = `${divSize}px`;
       div.style.height = `${divSize}px`;
-      div.addEventListener("mouseover", (e) =>
-        e.target.classList.add("my-color-class")
-      );
+
+      div.addEventListener("mouseover", () => {
+        randomColor(div);
+      });
       divRows.appendChild(div);
     }
     divContainer.appendChild(divRows);
   }
 }
-createDivs();
+
+function randomColor(div) {
+  let r = Math.floor(Math.random() * 255);
+  let g = Math.floor(Math.random() * 255);
+  let b = Math.floor(Math.random() * 255);
+  div.style.backgroundColor = `rgb(${r},${g},${b})`;
+}
+
+const slider = document.querySelector(".slider");
+const sizeText = document.querySelector("p");
+
+slider.addEventListener("change", () => {
+  createDivs(slider.value);
+  sizeText.textContent = `${slider.value} * ${slider.value}`;
+});
+
+createDivs(slider.value);
 container.appendChild(divContainer);
